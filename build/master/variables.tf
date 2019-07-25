316c70b2895af285e15688f589e4fbc538ce8f62
Sander Fearnley Hvas <sander@mac.internal.yoti.com>
2019-07-25 16:50:24 +0100

    added some backend.tf and build for cloud build testing

---
+++ build/master/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  default = "europe-west1"
+}
+
+variable "zone" {
+  default = "europe-west1-a"
+}
+
+variable "project-name" {
+  default = "active-incline-247715"
+}
+
+variable "location" {
+  default = "EU"
+}
+
+variable "name" {
+  type = "string"
+}
+
+variable "machine_type" {
+  type = "string"
+  default = "n1-standard-1"
+}
